2f4b9b0918681947c4d7d229690ce673b29d90f9
noah <noah.lee@buzzvil.com>
2021-12-25 10:32:01 +0900

    Add example

--- test/main.go
+++ test/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"time"
+
+	"github.com/gitploy-io/cronexpr"
 )
 
+// verifyFreezeWindow verifies the time satisfy being between two cronexpr.
+func verifyFreezeWindow(begin, end string, t time.Time) (bool, error) {
+	b, err := cronexpr.Parse(begin)
+	if err != nil {
+		return false, err
+	}
+
+	e, err := cronexpr.Parse(end)
+	if err != nil {
+		return false, err
+	}
+
+	if !(t.Before(b.Prev(t)) && t.After(e.Next(t))) {
+		return false, nil
+	}
+
+	return true, nil
+}
+
 func main() {
-	t := time.Date(2021, 6, 0, 23, 59, 59, 0, time.UTC)
-	fmt.Println(t)
+	t := time.Date(2021, 12, 25, 0, 0, 0, 0, time.UTC)
 
-	t = time.Date(t.Year(), t.Month(), 0, 23, 59, 59, 0, time.UTC)
-	fmt.Println(t)
-
-	t = time.Date(t.Year(), t.Month(), 0, 23, 59, 59, 0, time.UTC)
-	fmt.Println(t)
+	ok, _ := verifyFreezeWindow("* 23 24 DEC *", "* 1 25 DEC *", t)
+	if !ok {
+		log.Printf("Blocked to deploy at the midnight of X-mas.")
+	}
 }
